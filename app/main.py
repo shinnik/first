f36234a056f17a34d103070762161fa70cc02fc4
Nikita Izrailev <izrailev@phystech.edu>
2018-10-15 18:50:04 +0300

    uri instead of url

--- app/main.py
+++ app/main.py
@@ -6,8 +6,9 @@ def wsgi_application(environ, start_response):
 	status = '200 OK'
 	headers = [('content-type', 'application/json')]
 	time = str(datetime.datetime.now().time())
+	uri = "".join([environ["HTTP_HOST"], environ["RAW_URI"]])
 	info = json.dumps(
-	{"data": {"time": time, "url": 			environ["HTTP_HOST"].encode("utf-8")},
+	{"data": {"time": time, "uri": uri},
 	"content-type": "application/json"}
 	)
 	start_response(status, headers)
