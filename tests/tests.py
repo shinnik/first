fbeeaa3e0720f1cc89d98499639878888cc83e8b
Nikita Izrailev <izrailev@phystech.edu>
2018-11-14 01:34:41 +0300

    tests added

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,9 @@
 from urllib import request
+import requests as req
 from flask import jsonify
 import unittest
 from jsondiff import diff
+import json
 import sys
 import os
 
@@ -22,36 +24,6 @@ change_path_to_import_app()
 from app import app
 
 
-def api_post(api_name, datatype):
-
-    data = b'this_is_post_request'
-    req = request.urlopen(f'http://127.0.0.1:5050/api/{api_name}/', data)
-
-    if datatype == 'code':
-        return req.getcode()
-    if datatype == 'mime-type':
-        return req.info()['Content-Type']
-    if datatype == 'data':
-        with req as resp:
-            result_json = resp.read().decode('utf-8')
-            return result_json
-
-
-def api_get(api_name, get_params, datatype):
-
-    data = None
-    req = request.urlopen(f'http://127.0.0.1:5050/api/{api_name}/?{get_params}', data)
-
-    if datatype == 'code':
-        return req.getcode()
-    if datatype == 'mime-type':
-        return req.info()['Content-Type']
-    if datatype == 'data':
-        with req as resp:
-            result_json = resp.read().decode('utf-8')
-            return result_json
-
-
 def are_jsons_same(json1, json2):
     result = diff(json1, json2)
     if result == {}:
@@ -60,229 +32,77 @@ def are_jsons_same(json1, json2):
         return False
 
 
-class TestStubs(unittest.TestCase):
+class TestAPI(unittest.TestCase):
 
         def test_search_users(self):
             with app.app_context():
 
                 api = 'search_users'
-                params = 'query=1&limit=5'
+                params = 'query=li&limit=5'
 
-                user1 = {
-                    'user_id': 1,
-                    'nick': 'Septienna',
-                    'name': 'Vokial',
-                    'avatar': 'avatar1.png'
+                expected_data = {
+                    "0": {
+                        "avatar": None,
+                        "name": "Asya",
+                        "nick": "lisa",
+                        "user_id": 2
+                    }
                 }
 
-                user2 = {
-                    'user_id': 2,
-                    'nick': 'Lord Haart',
-                    'name': 'Sandro',
-                    'avatar': 'avatar2.png'
-                }
+                r = req.get(f'http://127.0.0.1:5050/api/{api}/?{params}')
+                obtained_data = json.loads(r.content.decode('utf-8'))
 
-                expected_data = jsonify({"users": [user1, user2]}).data.decode('utf-8')
-                obtained_data = api_get(api, params, 'data')
-
-                self.assertEqual(api_get(api, params, 'code'), 200)
-                self.assertEqual(api_get(api, params, 'mime-type'), 'application/json')
+                self.assertEqual(r.status_code, 200)
+                self.assertEqual(r.headers["Content-Type"], 'application/json')
                 self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_search_chats(self):
-            with app.app_context():
-
-                api = 'search_chats'
-                params = 'query=1&limit=5'
-
-                chat1 = {
-                    'chat_id': 1,
-                    'is_group_chat': True,
-                    'topic': 'Necropolis',
-                    'last_message': 'I am vampire lord',
-                    'new_messages': 14,
-                    'last_read_message_id': 149
-                }
-
-                chat2 = {
-                    'chat_id': 2,
-                    'is_group_chat': False,
-                    'topic': 'Castle',
-                    'last_message': 'I am looking forward to Monday',
-                    'new_messages': 11,
-                    'last_read_message_id': 87
-                }
-
-                expected_data = jsonify({"chats": [chat1, chat2]}).data.decode('utf-8')
-                obtained_data = api_get(api, params, 'data')
-
-                self.assertEqual(api_get(api, params, 'code'), 200)
-                self.assertEqual(api_get(api, params, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
 
         def test_get_chat_list(self):
             with app.app_context():
 
                 api = 'list_chats'
-                params = ''
+                params = 'user_id=4'
 
-                chat1 = {
-                    'chat_id': 1,
-                    'is_group_chat': True,
-                    'topic': 'Necropolis',
-                    'last_message': 'I am vampire lord',
-                    'new_messages': 14,
-                    'last_read_message_id': 149
-                }
+                expected_data = {
+                    '0': {
+                        'chat_id': 10,
+                        'is_group_chat': False,
+                        'last_message': 'hello',
+                        'last_read_message_id': 28,
+                        'new_messages': None,
+                        'topic': ''
+                    }}
 
-                chat2 = {
-                    'chat_id': 2,
-                    'is_group_chat': False,
-                    'topic': 'Castle',
-                    'last_message': 'I am looking forward to Monday',
-                    'new_messages': 11,
-                    'last_read_message_id': 87
-                }
+                r = req.get(f'http://127.0.0.1:5050/api/{api}/?{params}')
+                obtained_data = json.loads(r.content.decode('utf-8'))
 
-                expected_data = jsonify({"chats": [chat1, chat2]}).data.decode('utf-8')
-                obtained_data = api_get(api, params, 'data')
-
-                self.assertEqual(api_get(api, params, 'code'), 200)
-                self.assertEqual(api_get(api, params, 'mime-type'), 'application/json')
+                self.assertEqual(r.status_code, 200)
+                self.assertEqual(r.headers["Content-Type"], 'application/json')
                 self.assertTrue(are_jsons_same(obtained_data, expected_data))
 
         def test_create_pers_chat(self):
             with app.app_context():
 
+                user_id = 2
+
                 api = 'create_pers_chat'
-                params = 'user_id=3'
 
-                chat = {
-                    'chat_id': 3,
-                    'is_group_chat': False,
-                    'topic': 'Rampart',
-                    'last_message': 'Centaurus win',
-                    'new_messages': 41,
-                    'last_read_message_id': 55
+                expected_data = {
+                    "chat_id": 1,
+                    "is_group_chat": False,
+                    "last_message": "",
+                    "last_read_message_id": None,
+                    "new_messages": None,
+                    "topic": ""
                 }
 
-                expected_data = jsonify({"chat": chat}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
+                r = req.post(f'http://127.0.0.1:5050/api/{api}/', data={"companion_id": 3})
+                obtained_data = json.loads(r.content.decode('utf-8'))
 
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
+                self.assertEqual(r.status_code, 200)
+                self.assertEqual(r.headers["Content-Type"], 'application/json')
                 self.assertTrue(are_jsons_same(obtained_data, expected_data))
 
-        def test_create_group_chat(self):
-            with app.app_context():
-                api = 'create_group_chat'
-                params = 'user_id=3'
 
-                chat = {
-                    'chat_id': 4,
-                    'is_group_chat': False,
-                    'topic': 'Necropolis',
-                    'last_message': 'We need to kick Septienna off',
-                    'new_messages': 111,
-                    'last_read_message_id': 201
-                }
-
-                expected_data = jsonify({"chat": chat}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
-
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_add_members_to_group_chat(self):
-            with app.app_context():
-
-                api = 'add_members_to_group_chat'
-                params = 'user_id=3'
-
-                expected_data = jsonify({}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
-
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_leave_group_chat(self):
-            with app.app_context():
-
-                api = 'leave_group_chat'
-                params = 'user_id=3'
-
-                expected_data = jsonify({}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
-
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_send_message(self):
-            with app.app_context():
-
-                api = 'send_message'
-
-                message = {
-                    'message_id': '198',
-                    'chat_id': 'chat_id',
-                    'user_id': '22',
-                    'content': 'content',
-                    'added_at': '1540198594'
-                }
-
-                expected_data = jsonify({'message': message}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
-
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_read_message(self):
-            with app.app_context():
-
-                api = 'read_message'
-                params = 'message_id=198'
-
-                chat = {
-                    'chat_id': '5',
-                    'is_group_chat': 'False',
-                    'topic': 'Inferno',
-                    'last_message': 'Get through gate of hell',
-                    'new_messages': '123',
-                    'last_read_message_id': '321'
-                }
-
-                expected_data = jsonify({'chat': chat}).data.decode('utf-8')
-                obtained_data = api_get(api, params, 'data')
-
-                self.assertEqual(api_get(api, params, 'code'), 200)
-                self.assertEqual(api_get(api, params, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
-
-        def test_load_file(self):
-            with app.app_context():
-
-                api = 'upload_file'
-
-                attach = {
-                    'attach_id': '1',
-                    'message_id': '155',
-                    'chat_id': '5',
-                    'user_id': '44',
-                    'type': 'image',
-                    'url': 'pics/r83Jdf1Jd38d912n.jpg'
-                }
-
-                expected_data = jsonify({'attach': attach}).data.decode('utf-8')
-                obtained_data = api_post(api, 'data')
-
-                self.assertEqual(api_post(api, 'code'), 200)
-                self.assertEqual(api_post(api, 'mime-type'), 'application/json')
-                self.assertTrue(are_jsons_same(obtained_data, expected_data))
 
 
 if __name__ == "__main__":
