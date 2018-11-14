04a02242894a2d940df57f682c8ffec3609ff73d
Nikita Izrailev <izrailev@phystech.edu>
2018-11-14 15:09:21 +0300

    added validation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from .instance.config import TestConfig, ProductConfig
+from flask_jsonrpc import JSONRPC
 
 
 app = Flask(__name__)
 app.config.from_object(TestConfig)
+jsonrpc = JSONRPC(app, "/api")
 
-from .views import *
+
+from .handlers import *
--- app/handlers.py
+++ app/handlers.py
@@ -4,38 +4,38 @@ from app import model
 from app import app
 
 
-@jsonrpc.method('list_chats(user_id=Number) -> Object')
+@jsonrpc.method('list_chats(user_id=Number) -> Object', validate=True)
 def chats(user_id):
     chats = model.list_chats(user_id)
     return jsonify(chats)
 
 
-@jsonrpc.method('search_users(query=String, limit=Number) -> Object')
+@jsonrpc.method('search_users(query=String, limit=Number) -> Object', validate=True)
 def search_users(query, limit):
     users = model.get_users_by_query(query, limit)
     return jsonify(users)
 
 
-@jsonrpc.method('create_pers_chat(user_id=Number, companion_id=Number) -> Object')
+@jsonrpc.method('create_pers_chat(user_id=Number, companion_id=Number) -> Object', validate=True)
 def create_pers_chat(user_id, companion_id):
     # user_id will be taken from authorization
     rv = model.create_p_chat(user_id, companion_id)
     return jsonify(rv)
 
 
-@jsonrpc.method('send_message(user_id=Number, chat_id=Number, content=String) -> Object')
+@jsonrpc.method('send_message(user_id=Number, chat_id=Number, content=String) -> Object', validate=True)
 def send_message(user_id, chat_id, content):
     rv = model.send(user_id, chat_id, content)
     return jsonify(rv)
 
 
-@jsonrpc.method('list_messages(chat_id=Number, limit=Number) -> Object')
+@jsonrpc.method('list_messages(chat_id=Number, limit=Number) -> Object', validate=True)
 def list_messages(chat_id, limit):
     rv = model.list_messages_by_chat(chat_id, limit)
     return jsonify(rv)
 
 
-@jsonrpc.method('read_message(user_id=Number, message_id=Number) -> Object')
+@jsonrpc.method('read_message(user_id=Number, message_id=Number) -> Object', validate=True)
 def read(user_id, message_id):
     rv = model.read(user_id, message_id)
     return jsonify(rv)
